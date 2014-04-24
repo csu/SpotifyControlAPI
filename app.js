8d9ffe7e04e658200ed27553e1ba2461ac52175e
Christopher Su <christophersu9@gmail.com>
2014-04-23 20:30:18 -0700

    now working on express 4

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var spotify = require('spotify-node-applescript');
- 
-var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/play/:uri', function(req, res) {
-    spotify.playTrack(req.params.uri, function(){
-    // track is playing
-    });
-    res.send(200, {uri: req.params.uri});
+var app = express();
+app.use(bodyParser());
+
+var router = express.Router();
+
+router.get('/play/:uri', function(req, res) {
+    spotify.playTrack(req.params.uri, function(){});
+    res.send(200, {reponse: "it worked!"});
 });
- 
-app.listen(3000);
-console.log('Listening on port 3000...');
+
+router.get('/play', function(req, res) {
+    spotify.playTrack(req.body.uri, function(){});
+    res.send(200, req.body);
+});
+
+router.get('/', function(req, res) {
+    res.sendfile('index.html')
+});
+
+app.use('/api', router);
+
+var port = 3000;
+app.listen(port);
+console.log('Magic happens on port ' + port);
