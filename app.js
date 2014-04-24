e268d56320a91ccf840a77d185228518c65b8fd9
Christopher Su <christophersu9@gmail.com>
2014-04-23 21:11:07 -0700

    now redirects to front page after gui

--- app.js
+++ app.js
@@ -7,15 +7,26 @@ app.use(bodyParser());
 
 var router = express.Router();
 
+function playSong(uri) {
+    spotify.playTrack(uri, function(){});
+}
+
 router.get('/play/:uri', function(req, res) {
-    spotify.playTrack(req.params.uri, function(){});
-    res.send(200, {reponse: "it worked!"});
+    playSong(req.params.uri);
+    res.sendfile('redirect.html')
+    // res.send(200, {reponse: "it worked!"});
 });
 
 router.post('/play', function(req, res) {
-    console.log(req.body)
-    spotify.playTrack(req.body.uri, function(){});
-    res.send(200, req.body.uri);
+    playSong(req.body.uri);
+    res.sendfile('redirect.html')
+    // res.send(200, {response: "now playing " + req.body.uri});
+});
+
+router.post('/', function(req, res) {
+    playSong(req.body.uri);
+    res.sendfile('redirect.html')
+    // res.send(200, {response: "now playing " + req.body.uri});
 });
 
 router.get('/', function(req, res) {
